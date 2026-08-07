5f821d9a8eec83a91eaa6c26c277a66f07c8c63c
Maurice Laveaux <m.laveaux@tue.nl>
2026-08-07 15:12:43 +0200

    Make trycatch also catch generic errors, not derived from std::exception

--- cpp/exception.h
+++ cpp/exception.h
@@ -13,8 +13,8 @@ namespace rust::behavior {
 // let exceptions propagate normally. Meaning they will be converted to Rust `Result` without stack traces.
 #ifdef MCRL2_ENABLE_CPPTRACE
   template <typename Try, typename Fail>
-  static void trycatch(Try &&func, Fail &&fail) noexcept 
-  { 
+  static void trycatch(Try &&func, Fail &&fail) noexcept
+  {
     CPPTRACE_TRY {
       func();
     } CPPTRACE_CATCH(const std::exception &e) {
@@ -23,6 +23,12 @@ namespace rust::behavior {
       }
 
       fail(e.what());
+    } catch (...) {
+      // This function is noexcept, so anything not derived from std::exception
+      // would terminate the process instead of being reported to Rust. mCRL2
+      // only throws std::* exceptions, but report it as an error rather than
+      // crashing if that ever changes.
+      fail("Unknown C++ exception (not derived from std::exception)");
     }
   }
 #endif // MCRL2_ENABLE_CPPTRACE
